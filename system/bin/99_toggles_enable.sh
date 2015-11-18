7bd684210fa024a129cf6d802393e782dd2e38c6
Gabri98 <gabriele.m1798@gmail.com>
2015-11-18 09:16:55 +0100

    removed toggle tomato

--- system/bin/99_toggles_enable.sh
+++ system/bin/99_toggles_enable.sh
@@ -5,7 +5,7 @@ mount -o rw,remount /system
 
 if [ -e /data/data/com.android.providers.settings/databases/settings.db ]; 
  	then
- 	/system/xbin/sqlite3 /data/data/com.android.providers.settings/databases/settings.db "update system set value='Wifi;Location;MobileData;SilentMode;Tomato;AutoRotate;Bluetooth;PowerSaving;TorchLight;AirplaneMode;DormantMode;UltraPowerSaving;WiFiHotspot;SmartStay;PersonalMode;AllShareCast;Nfc;Sync;MultiWindow;FloatingMessage;SFinder;QuickConnect;SideKey;NetworkBooster;TouchSensitivity;' where name='notification_panel_active_app_list'";
+ 	/system/xbin/sqlite3 /data/data/com.android.providers.settings/databases/settings.db "update system set value='Wifi;Location;MobileData;SilentMode;AutoRotate;Bluetooth;PowerSaving;TorchLight;AirplaneMode;DormantMode;UltraPowerSaving;WiFiHotspot;SmartStay;PersonalMode;AllShareCast;Nfc;Sync;MultiWindow;FloatingMessage;SFinder;QuickConnect;SideKey;NetworkBooster;TouchSensitivity;' where name='notification_panel_active_app_list'";
 fi
 if [ -e /data/data/com.android.providers.settings/databases/settings.db ]; 
 	then
